c0e450e60da8bf543139ce110fe2c13b43f605c2
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-26 19:02:59 +0100

    Add update_yamls_from_upstream.sh to tekton

---
+++ tekton/update_yamls_from_upstream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# Script to update tekton yaml files form upstream
+
+curl -L https://github.com/tektoncd/dashboard/releases/download/v0.4.1/dashboard_latest_release.yaml > templates/tekton-dashboard.yaml
+
+curl -L https://github.com/tektoncd/pipeline/releases/download/v0.10.0/release.yaml | sed \
+  -e '17 a\ \ labels:\n\ \ \ \ app: kubed' \
+> templates/tekton-pipelines.yaml

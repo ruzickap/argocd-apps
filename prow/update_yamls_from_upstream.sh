73ae542093c6298cc7a35c6e52f9a10a8882da61
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-27 10:40:19 +0100

    Add ingress for tekton

--- prow/update_yamls_from_upstream.sh
+++ prow/update_yamls_from_upstream.sh
@@ -2,8 +2,11 @@
 
 # Script to update prow yaml files form upstream
 
-curl https://raw.githubusercontent.com/kubernetes/test-infra/master/prow/cluster/starter.yaml | sed \
+curl https://raw.githubusercontent.com/kubernetes/test-infra/master/prow/cluster/starter.yaml > templates/starter.yaml-upstream
+
+sed \
   -e 's/namespace: default/namespace: {{ $.Values.namespace }}/' \
   -e 's/prowjob_namespace: default/prowjob_namespace: {{ $.Values.prowjob_namespace }}/' \
   -e 's/: test-pods/: {{ $.Values.prowjob_namespace }}/' \
-> templates/starter.yaml
+templates/starter.yaml-upstream | \
+tac | sed '/          servicePort: 8888/,/^\-\-\-$/d' | tac > templates/prow.yaml

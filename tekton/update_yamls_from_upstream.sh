8b6e09d9793fa4f0603932a492460145fe857d4a
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-30 10:00:29 +0100

    Add variables with versions to tekton script

--- tekton/update_yamls_from_upstream.sh
+++ tekton/update_yamls_from_upstream.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -eux
 
+TEKTON_DASHBOARD_VERSION="v0.4.1"
+TEKTON_PIPELINE_VERSION="v0.10.1"
+
 # Script to update tekton yaml files form upstream
 
-curl -L https://github.com/tektoncd/dashboard/releases/download/v0.4.1/dashboard_latest_release.yaml > templates/tekton-dashboard.yaml
+curl -L https://github.com/tektoncd/dashboard/releases/download/${TEKTON_DASHBOARD_VERSION}/dashboard_latest_release.yaml > templates/tekton-dashboard.yaml
 
-curl -L https://github.com/tektoncd/pipeline/releases/download/v0.10.1/release.yaml | sed \
+curl -L https://github.com/tektoncd/pipeline/releases/download/${TEKTON_PIPELINE_VERSION}/release.yaml | sed \
   -e '17 a\ \ labels:\n\ \ \ \ app: kubed' \
 > templates/tekton-pipelines.yaml

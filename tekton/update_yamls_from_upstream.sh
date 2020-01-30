f2289a4c3bc64256a65c7cd61ad9dc7437c60d7f
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-30 09:43:50 +0100

    Updating tekton to v0.10.1

--- tekton/update_yamls_from_upstream.sh
+++ tekton/update_yamls_from_upstream.sh
@@ -4,6 +4,6 @@
 
 curl -L https://github.com/tektoncd/dashboard/releases/download/v0.4.1/dashboard_latest_release.yaml > templates/tekton-dashboard.yaml
 
-curl -L https://github.com/tektoncd/pipeline/releases/download/v0.10.0/release.yaml | sed \
+curl -L https://github.com/tektoncd/pipeline/releases/download/v0.10.1/release.yaml | sed \
   -e '17 a\ \ labels:\n\ \ \ \ app: kubed' \
 > templates/tekton-pipelines.yaml

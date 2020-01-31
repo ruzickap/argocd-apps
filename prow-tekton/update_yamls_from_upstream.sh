3266acb3ae9dfced429cd50c8fd821f584ecde0c
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-31 09:55:27 +0100

    Downgrade tekton to version supported by prow

--- prow-tekton/update_yamls_from_upstream.sh
+++ prow-tekton/update_yamls_from_upstream.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -eux
 
 TEKTON_DASHBOARD_VERSION="v0.4.1"
-TEKTON_PIPELINE_VERSION="v0.10.1"
+# Use tekton pipeline version which is supported by prow !
+# https://github.com/kubernetes/test-infra/blob/master/go.mod#L92
+TEKTON_PIPELINE_VERSION="v0.8.0"
 
 # Script to update tekton yaml files form upstream
 
 curl -sL https://github.com/tektoncd/dashboard/releases/download/${TEKTON_DASHBOARD_VERSION}/dashboard_latest_release.yaml > templates/tekton-dashboard.yaml
 
 curl -sL https://github.com/tektoncd/pipeline/releases/download/${TEKTON_PIPELINE_VERSION}/release.yaml | sed \
-  -e '17 a\ \ labels:\n\ \ \ \ app: kubed' \
+  -e '4 a\ \ labels:\n\ \ \ \ app: kubed' \
 > templates/tekton-pipelines.yaml
 
 

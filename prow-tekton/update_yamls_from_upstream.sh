d951dd262b918f8250b180261f28284211ed7471
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-02-07 09:22:58 +0100

    Adding updated prow which should contain support for Tekton 0.10.1
    Details: https://github.com/kubernetes/test-infra/issues/16120

--- prow-tekton/update_yamls_from_upstream.sh
+++ prow-tekton/update_yamls_from_upstream.sh
@@ -3,14 +3,14 @@
 TEKTON_DASHBOARD_VERSION="v0.5.0"
 # Use tekton pipeline version which is supported by prow !
 # https://github.com/kubernetes/test-infra/blob/master/go.mod#L92
-TEKTON_PIPELINE_VERSION="v0.8.0"
+TEKTON_PIPELINE_VERSION="v0.10.1"
 
 # Script to update tekton yaml files form upstream
 
 curl -sL https://github.com/tektoncd/dashboard/releases/download/${TEKTON_DASHBOARD_VERSION}/tekton-dashboard-release.yaml > templates/tekton-dashboard.yaml
 
 curl -sL https://github.com/tektoncd/pipeline/releases/download/${TEKTON_PIPELINE_VERSION}/release.yaml | sed \
-  -e '4 a\ \ labels:\n\ \ \ \ app: kubed' \
+  -e '17 a\ \ labels:\n\ \ \ \ app: kubed' \
 > templates/tekton-pipelines.yaml
 
 
